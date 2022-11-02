e0d80b76b6276f4876206fab16062240aebf4576
gabrielovski <gabriel8.cavalcante@gmail.com>
2022-11-02 18:30:19 -0300

    criação de uma variável que muda o alt

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 function createGame(player1, hour, player2) {
+  let player1alt = player1.slice(-1) === "a" ? "da" : "do"
+  let player2alt = player2.slice(-1) === "a" ? "da" : "do"
   return `
     <li>
-        <img src="./assets/${player1}.svg" alt="Bandeira do ${player1}" />
+        <img src="./assets/${player1}.svg" alt="Bandeira ${player1alt} ${player1}" />
         <strong>${hour}</strong>
-        <img src="./assets/${player2}.svg" alt="Bandeira do ${player2}" />
+        <img src="./assets/${player2}.svg" alt="Bandeira ${player2alt} ${player2}" />
     </li>
   `
 }
