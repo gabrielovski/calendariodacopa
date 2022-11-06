5a98ffbe80ec4e1be9608f1afdfd8fb5622d2fa0
gabrielovski <gabriel8.cavalcante@gmail.com>
2022-11-05 22:32:21 -0300

    mudanças no gap da ul, no footer e identação no JS

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ function createGame(player1, hour, player2) {
   let player2alt = player2.slice(-1) === "a" ? "da" : "do"
   return `
     <li>
-        <img src="./assets/${player1}.svg" alt="Bandeira ${player1alt} ${player1}" />
-        <strong>${hour}</strong>
-        <img src="./assets/${player2}.svg" alt="Bandeira ${player2alt} ${player2}" />
+      <img src="./assets/${player1}.svg" alt="Bandeira ${player1alt} ${player1}" />
+      <strong>${hour}</strong>
+      <img src="./assets/${player2}.svg" alt="Bandeira ${player2alt} ${player2}" />
     </li>
     <div class="nome-times">
-    <h1>${player1}</h1>
-    <h1>${player2}</h1>
+      <h1>${player1}</h1>
+      <h1>${player2}</h1>
     </div>
   `
 }
